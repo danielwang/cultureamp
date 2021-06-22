1fc4327042926ebb014efa298042893627161d9f
Daniel Wang <daniel@campaignagent.com.au>
2021-06-22 16:27:29 +1000

    feat: import bootstrap 5 sass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
 
-import './index.css';
+import './sass/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
